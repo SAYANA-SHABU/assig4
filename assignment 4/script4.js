7278c2d9163b6a835cc89417e8f537b03ef96805
SAYANA-SHABU <144515427+SAYANA-SHABU@users.noreply.github.com>
2024-02-09 19:11:45 +0530

    script4.js

--- assignment 4/script4.js
+++ assignment 4/script4.js
@@ -1,1 +1,24 @@
 // Write a JavaScript function to get an array and to read the first element of an array. Check whether it is prime or not. 
+function isPrime(num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkPrimes(arr) {
+    let primeStatus = [];
+    for (let i = 0; i < arr.length; i++) {
+        primeStatus.push(isPrime(arr[i]));
+    }
+    return primeStatus;
+}
+
+
+let array = [7, 4, 9, 11];
+console.log("Prime status of each element:[17, 4, 9, 11]=", checkPrimes(array));
